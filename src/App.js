7457ed1433aa55d0db839021038ce675a6371a24
Karin Lampesberger <karin@lampesberger.at>
2020-10-18 15:22:02 +0200

    Imported timeline react component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import {Timeline, TimelineEvent} from 'react-event-timeline'
+import Button from 'react-bootstrap/Button';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="jumbotron text-center">
+        <h1 className="display-4">Covid-19 in Deutschland</h1>
+        <div>
+        <p>Scrolle runter um mehr zu sehen</p>
+<Button>Test </Button>
+        </div>
+      </div>
+      <div className="container">
+        <Timeline>
+
+                <TimelineEvent title="Erster Coronafall"
+                               createdAt="27.01.2020"
+                               >
+                    Erster Coronafall in Deutschland (Bayern, Starnberg).
+                </TimelineEvent>
+
+                <TimelineEvent
+                    title="Corona Rückkehrer aus Wuhan"
+                    createdAt="01.02.2020"
+                    >
+                 Eine Reisegruppe kehr aus Wuhan zurück und muss sich umgehend in Quarantäne begeben.
+                </TimelineEvent>
+        </Timeline>
+      </div>
+    </>
   );
 }
 
